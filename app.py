c1f6ec4c44619dd22a8d132df6d2475b8ca28f9f
pedrovsfelix <pedrovitorsf@gmail.com>
2024-12-13 18:50:24 -0300

    :sparkles: feat: Inserindo as funções de pesquisa de arquivo

--- app.py
+++ app.py
@@ -16,14 +16,22 @@ class App:
         CTkLabel(self.root, text='Gere termos de forma automática', font=("Arial", 18)).place(x=50, y=0)
         
         self.label_filelist = CTkLabel(self.root, text='', font=("Arial", 12)).place(x=10, y=40)
-        CTkButton(self.root, width=320, height=40, text='Buscar Lista', font=("Arial", 21)).place(x=20, y=70)
+        CTkButton(self.root, width=320, height=40, command=self.browserList, text='Buscar Lista', font=("Arial", 21)).place(x=20, y=70)
         
         self.label_filedocx = CTkLabel(self.root, text='', font=("Arial", 12)).place(x=10, y=40)
         CTkButton(self.root, width=320, height=40, text='Buscar Termo', font=("Arial", 21)).place(x=20, y=150)
         
         CTkButton(self.root, width=320, height=40, text='Gerar Termos', font=("Arial", 21)).place(x=20, y=240)
-        
+    
+    def browserList(self):
+        file = filedialog.askopenfile(initialdir="C:/Users/pedro.felix/Documents/Projects/Python/TermsBot/", title="Selecione o arquivo da lista", filetypes=(("Excel Files", "*.xlsx*"), ("All Files", "*.*")))
+        if file:
+            self.label_filelist.configure(text=file.name)
 
+    def browserTerms(self):
+        file = filedialog.askopenfile(initialdir="C:/Users/pedro.felix/Documents/Projects/Python/TermsBot/", title="Selecione o arquivo do termo", filetypes=(("Docx Files", "*.docx*"), ("All Files", "*.*")))
+        if file:
+            self.label_filelist.configure(text=file.name)
 
 if __name__ == "__main__":
     app = App()
